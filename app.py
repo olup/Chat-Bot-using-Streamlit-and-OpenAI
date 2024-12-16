f5b4823d3f302197146a7b986760e357515a5fdf
Olup <olup@users.noreply.github.com>
2024-12-16 08:30:35 +0000

    update

--- app.py
+++ app.py
@@ -87,18 +87,12 @@ if "openai_model" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-st.subheader("History")
-st.code(st.session_state.summarized_history)
-
-st.subheader("State")
-st.code(st.session_state.conversation_state)
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
-
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -147,3 +141,13 @@ We are in a middle of a conversation.
             response,
         )
     )
+
+st.markdown(
+    f"""
+            **Summary**
+            {st.session_state.summarized_history}
+
+            **Conversation State**
+            {st.session_state.conversation_state}
+            """
+)
